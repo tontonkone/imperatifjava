c5cdd1a5405094a6a8d33cbe168e0723f5954067
mamoudou <konemamoudou84@gmail.com>
2023-05-16 11:19:20 +0200

    test 2

---
+++ TP03/exostructure/src/fr/boucles/ExerciceBoucleBase.java
@@ -0,0 +1,16 @@
+package fr.boucles;
+
+public class ExerciceBoucleBase {
+
+    public static void main(String[] args){
+
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println(i);
+        }
+        // afficher nom
+        for (int i = 0; i <= 20 ; i++) {
+            String nom = "kone", prenom = "mamoudou";
+            System.out.println(nom + " " + prenom);
+        }
+    }
+}
