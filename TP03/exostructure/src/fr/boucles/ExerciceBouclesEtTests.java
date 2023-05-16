f6dd773d3b9653d5811fa1d880dcbab84f750d10
mamoudou <konemamoudou84@gmail.com>
2023-05-16 13:17:51 +0200

    exo discours

---
+++ TP03/exostructure/src/fr/boucles/ExerciceBouclesEtTests.java
@@ -0,0 +1,23 @@
+package fr.boucles;
+
+public class ExerciceBouclesEtTests {
+
+    public static void main(String[] args){
+
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+
+        for (int i = array.length - 1 ; i > -1 ; i--) {
+            System.out.println("Inverser les elements :" + array[i]);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > 3)
+                System.out.println(array[i]);
+        }
+    }
+}
--- TP03/exostructure/src/fr/boucles/ExerciceTableauEntier.java
+++ TP03/exostructure/src/fr/boucles/ExerciceTableauEntier.java
@@ -4,6 +4,11 @@ public class ExerciceTableauEntier {
 
     public static  void main(String[] args){
 
-
+        int[] tab = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(" Affichez le premier élément du tableau : " + tab[0]);
+        System.out.println("Affichez la longueur du tableau en utilisant la propriété length :" + tab.length);
+        System.out.println(" Affichez le dernier élément du tableau en utilisant la propriété length :" + tab[tab.length - 1]);
+        tab[4] = 8;
+        System.out.println(tab[4]);
     }
 }
